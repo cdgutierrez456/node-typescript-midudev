8ad638d897ec1389b9f647b8125eab8f09089207
Cristian Gutierrez <cdgutierrez456@gmail.com>
2023-11-11 00:04:16 -0500

    Creando archivo de types donde hay enums y tipos. Adional el archivo de utils para hacer validaciones de valores que llegan desde alguna peticion

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -1,13 +1,29 @@
 import express from 'express';
+import * as diaryServices from '../services/diaries.service';
+import toNewDiaryEntry from '../utils';
 
 const router = express.Router()
 
 router.get('/', (_, res) => {
-  res.send('Fetching all entry diaries')
+  res.send(diaryServices.getEntriesWithoutSensitiveInfo())
 })
 
-router.post('/', (_, res) => {
-  res.send('Saving a diary!')
+router.get('/:id', (req, res) => {
+  const { id } = req.params
+  const diary = diaryServices.findById(+id)
+  return (diary != null)
+    ? res.send(diary)
+    : res.sendStatus(404)
+})
+
+router.post('/', (req, res) => {
+  try {
+    const newDiaryEntry = toNewDiaryEntry(req.body)
+    const addedDiaryEntry = diaryServices.addDiary(newDiaryEntry)
+    res.json(addedDiaryEntry)
+  } catch (e: any) {
+    res.status(400).send(e.message)
+  }
 })
 
 export default router;
---
+++ src/types.ts
@@ -0,0 +1,33 @@
+// Aqui es donde estan los tipos de la aplicacion
+ 
+// export type Visibility = 'great' | 'good' | 'ok' | 'poor';
+
+// export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy';
+
+export enum Weather {
+  Sunny = 'sunny',
+  Rainy = 'rainy',
+  Cloudy = 'cloudy',
+  Windy = 'windy',
+  Stormy = 'stormy'
+}
+
+export enum Visibility {
+  Great = 'great',
+  Good = 'good',
+  Ok = 'ok',
+  Poor = 'poor'
+}
+
+export interface DiaryEntry {
+  id: number
+  date: string
+  weather: Weather
+  visibility: Visibility
+  comment: string
+}
+
+// export type NonSensitiveInfoDiaryEntry = Pick<DiaryEntry, 'id' | 'date' | 'weather' | 'visibility'>
+
+export type NonSensitiveInfoDiaryEntry = Omit<DiaryEntry, 'comment'>
+export type NewDiaryEntry = Omit<DiaryEntry, 'id'>
